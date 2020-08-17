5de686414f6e6ef0e4595310af771cdbbe3d0b5f
sumith <sumith68@gmail.com>
2020-08-17 23:52:05 +0530

    Wire goodreads api to fetch books information

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,32 @@
 import React from "react";
 import SearchForm from "./components/SearchForm";
+var parseString = require("xml2js").parseString;
 
 function App() {
   let [search, setSearch] = React.useState("");
+  let [bookDetails, setBookDetails] = React.useState([]);
 
   let onSearchChange = (value: string) => {
     setSearch((prevValue) => value);
   };
 
   let onSearchSubmit = () => {
-    console.log("-------------------");
-    console.log(search);
-    console.log("-------------------");
+    let url = `https://cors-anywhere.herokuapp.com/https://www.goodreads.com/search/index?key=${process.env.REACT_APP_GOODREADS_API_KEY}&search=author&q=${search}`;
+    fetch(url, {
+      method: "GET",
+    })
+      .then((response) => response.text())
+      .then((response) => {
+        parseString(response, (err: any, result: any) => {
+          let searchResults =
+            result.GoodreadsResponse.search[0].results[0].work;
+          setBookDetails(searchResults);
+        });
+      });
   };
 
   return (
-    <div className="App">
+    <div>
       <div>
         <SearchForm
           search={search}
@@ -23,6 +34,20 @@ function App() {
           onSearchSubmit={onSearchSubmit}
         />
       </div>
+
+      {bookDetails.map((book: any) => (
+        <ul>
+          <li>{book.best_book[0].title[0]}</li>
+          <li>{book.best_book[0].author[0].name}</li>
+          <li>
+            <img
+              src={book.best_book[0].small_image_url[0]}
+              alt={book.best_book[0].title[0]}
+            />
+          </li>
+          <hr />
+        </ul>
+      ))}
     </div>
   );
 }
